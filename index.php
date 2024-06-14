0764f593393f9a2fe1245e8deaca9b1e34ebda87
Collins1402 <collins.kibiru@strathmore.edu>
2024-06-14 20:05:18 +0300

    Committed

---
+++ index.php
@@ -0,0 +1,40 @@
+<h1>Home Page</h1>
+<?php include('header.php'); ?>  <div class="container">
+    <h1>WELCOME TO STAN'S CAR ENTREPRISES</h1>
+    <p>Come and buy the fastest cars, with the best durability and can be used on all surfaces at affordable prices.</p>
+    
+    <p>We are esteemed to serve you with the highest quality of cars. Thank you.</p>
+
+    <p>
+      About the copyright issues, there are two solutions to the current dilemma you'll be in 
+      if you do not comply, and that is:
+    </p>
+    
+    <p>  1.] You will have to accept, without argument, that half of your earnings are now going to us.</p>
+  
+    <p>  2.] All your assets or at least 50% of your assets will now belong to us.</p>
+    
+    <p>That is if you want to keep quiet about it.
+      However, if you decide to go to court, we have documents signed by the judge and the governor,
+      that says;
+    </p>
+    
+    <p>     
+      "TO ANYONE WHO IS GUILTY OF COPYRIGHT ISSUES, A FINE OF 10 MILLION DOLLARS IS TO BE GIVEN
+      BACK TO THE COMPANY WHICH IS THE VICTIM HERE, THAT IS [STAN'S CAR ENTREPRISES]"
+    </p>
+    <p>BE CAUTIOUS!!!</p>
+    
+    <p>Here is a video for whatever is on our company</p>
+    <video src="videos/home page.mp4"></video>
+
+    <p>Check out some of what we sell to our customers:</p>
+  </div>
+  
+  <img src="Images/Audi1.jpeg" alt="width=10%">
+  <img src="Images/Bugatti1.jpeg" alt="width=10%">
+  <img src="Images/Chev1.jpeg" alt="width=10%">
+  <img src="Images/Chev_Camaro.jpeg" alt="width=10%">
+  <img src="Images/Dodge1.jpeg" alt="width=10%">
+  
+  <?php include('footer.php'); ?>
