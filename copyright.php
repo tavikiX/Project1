2f5bc092af25b6b9ecd97b5b54a0b19e4e0e8239
Collins1402 <collins.kibiru@strathmore.edu>
2024-06-14 20:18:07 +0300

    Commit

---
+++ copyright.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>  <h1>All rights reserved.</h1>
+<h2>DISCLAIMER:</h2>
+<p>
+  If this site is for some reason duplicated, 50% assets of the fraudulent company
+  will now be legally ours. Think twice!!!
+</p>
+
+<p>
+  To avoid this circumstance, a warning will be posted on your site about copyright
+  issues but if not dealt with within 24 hours, it will be deemed stolen and the case will
+  be dealt with upheld.
+</p>
+
+<p>For more information, visit our <a href="index.php">website</a>.</p>
+<?php include('footer.php'); ?>  
