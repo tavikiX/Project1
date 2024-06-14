e6647d75e8fe87a2228ffbd852c931ff04f6464a
Collins1402 <collins.kibiru@strathmore.edu>
2024-06-14 20:06:09 +0300

    Committed

--- help.php
+++ help.php
@@ -1,6 +1,6 @@
 <?php include('header.php'); ?>  <h1>Experiencing any problems</h1>
   <p>Please contact: 
-    <a href="tel:+254701004028">0701004028(Call)</a> | <a href="sms:+254701004028">0701004028(Message)</a>    
+    <a href="tel:+254701004028">0701004028(Call)</a> | <a href="sms:+254701004028">0701004028(Message)</a>
   </p>
   
   <?php include('footer.php'); ?> 
