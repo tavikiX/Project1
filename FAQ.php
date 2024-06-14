44985c72d944817099820b25de070bd7495cd8c2
Collins1402 <collins.kibiru@strathmore.edu>
2024-06-14 20:12:29 +0300

    Commit

---
+++ FAQ.php
@@ -0,0 +1,9 @@
+<?php include('header.php'); ?>  
+<h1>Any questions!!</h1>
+
+<p>When is the maintenance period?</p>
+<p>How long is the maintenance period?</p>
+<p>Who do I consult for problem solving?</p>
+
+<p>any further questions to be raised please search below:</p>
+<?php include('footer.php'); ?>
