387c2df3eac9cab39227acfb3a440e6659213103
Collins1402 <collins.kibiru@strathmore.edu>
2024-06-24 12:49:38 +0300

    committed

--- Database.sql
+++ Database.sql
@@ -1,33 +1,28 @@
-CREATE TABLE CAR
-(
-    CarType VARCHAR (50),
-    CarRegNo VARCHAR (25),
-    CarPrice int,
-    SellDate Date
+CREATE TABLE CAR (
+  CarType VARCHAR(50),
+  CarRegNo VARCHAR(25) PRIMARY KEY,  -- Added primary key constraint
+  CarPrice INT,
+  SellDate DATE
 );
 
-INSERT INTO PUBLIC.CAR
-(
-    CarType, CarRegNo, CarPrice, SellDate
+INSERT INTO CAR (
+  CarType, CarRegNo, CarPrice, SellDate
 )
 VALUES
-('Mercedes', 'KDE 865L', 5000000, '16-05-2023'),
-('Toyota', 'KCT 129G', 3800000, '18-05-2023'),
-('BMW','KDB 374K', 4000000, '20-05-2023');
+  ('Mercedes', 'KDE 865L', 5000000, '2023-05-16'),
+  ('Toyota', 'KCT 129G', 3800000, '2023-05-18'),
+  ('BMW', 'KDB 374K', 4000000, '2023-05-20');
 
-CREATE TABLE EMPLOYEE
-(
-    EmpID int,
-    EmpName VARCHAR(50),
-    EmpEmail VARCHAR(50),
+CREATE TABLE EMPLOYEE (
+  EmpID INT PRIMARY KEY, -- Added primary key constraint
+  EmpName VARCHAR(50),
+  EmpEmail VARCHAR(50)
 );
 
-INSERT INTO PUBLIC.EMPLOYEE
-(
-    EmpID, EmpName, EmpEmail
-);
-
+INSERT INTO EMPLOYEE (
+  EmpID, EmpName, EmpEmail
+)
 VALUES
-(19346723, 'Austin', 'austinF@Stan.com')
-(45362786, 'Phillip', 'phillipM@Stan.com')
-(56723323, 'Stanley', 'stanleyK@Stan.com');
+  (19346723, 'Austin', 'austinF@Stan.com'),
+  (45362786, 'Phillip', 'phillipM@Stan.com'),
+  (56723323, 'Stanley', 'stanleyK@Stan.com');
