aaecc3d697e3cca09e5867161e34ea06e8e6d1ee
Collins1402 <collins.kibiru@strathmore.edu>
2024-07-08 09:45:41 +0300

    Modified

--- Database.sql
+++ Database.sql
@@ -1,34 +1,18 @@
 CREATE TABLE CAR (
   CarType VARCHAR(50),
-  CarRegNo VARCHAR(25) PRIMARY KEY,  -- Added primary key constraint
+  CarRegNo VARCHAR(25) PRIMARY KEY,
   CarPrice INT,
   SellDate DATE
 );
 
-INSERT INTO CAR (
-  CarType, CarRegNo, CarPrice, SellDate
-)
-VALUES
-  ('Mercedes', 'KDE 865L', 5000000, 2023-05-16),
-  ('Toyota', 'KCT 129G', 3800000, 2023-05-18),
-  ('BMW', 'KDB 374K', 4000000, 2023-05-20);
-
 CREATE TABLE EMPLOYEE (
-  EmpID INT PRIMARY KEY, -- Added primary key constraint
+  EmpID INT PRIMARY KEY,
   EmpName VARCHAR(50),
   EmpEmail VARCHAR(50)
 );
 
-INSERT INTO EMPLOYEE (
-  EmpID, EmpName, EmpEmail
-)
-VALUES
-  (19346723, 'Austin', 'austinF@Stan.com'),
-  (45362786, 'Phillip', 'phillipM@Stan.com'),
-  (56723323, 'Stanley', 'stanleyK@Stan.com');
-
-  CREATE TABLE comments (
-  ID INT PRIMARY KEY AUTO_INCREMENT,  
+CREATE TABLE comments (
+  id INT PRIMARY KEY AUTO_INCREMENT,  
   username VARCHAR(255) NOT NULL,
   comment TEXT NOT NULL,
   rating INT,
